fb37858fcd19731a88c8a642f7e3f7901b5aab45
bryan <bry_angelo@yahoo.com>
2017-10-16 16:18:54 -0700

    added server

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const data = {
+  	"configurations": [
+  		{
+  			"name" : "host1",
+  			"hostname" : "nessus-ntp.lab.com",
+  			"port" : 1241,
+  			"username" : "toto"
+  		},
+  		{
+  			"name" : "host2",
+  			"hostname" : "nessus-xml.lab.com",
+  			"port" : 3384,
+  			"username" : "admin"
+  		}
+  	]
+}
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -7,6 +23,12 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
-app.get('/download', function (req, res) {
-  console.log(req);
-})
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/download/request', function (req, res) {
+  res.json(data.configurations.slice(0, req.query.host));
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  data : any;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+  	this.getData();
+  }
+
+  getData() {
+	this.dataService.getData().subscribe((res) => {
+		this.data = res;
+		console.log(res);
+	});
+  }
 }
